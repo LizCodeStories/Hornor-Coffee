8255db41b505c3d04adcd2f90ecf07ca1219b4f8
Elizabeth Salako <106440322+LizSalako@users.noreply.github.com>
2023-02-02 17:53:00 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello World');
+let chances = ["Congratulations! You won free coffee today! Take this screenshot to claim your free drink.",
+  "You claim 10% off your coffee today! Take this screenshot to claim 10% off your drink.",
+  "You have claimed 50% off your drink today! Take this screenshot to claim 50% off your drink.",
+  "You have claimed 15% off your drink today! Take this screenshot to claim 50% off your drink.",
+  "Sorry, you didn't win today but try again tomorrow!"
+]
+
+let button = document.getElementById('chance-button');
+let chance = document.getElementById('chance');
+
+function chanceSelector(){
+  let randomChance = Math.floor(Math.random() * chances.length);
+  return chances[randomChance];
+}
+
+function showChance(){
+  chance.innerHTML = chanceSelector();
+  button.innerHTML = "Come back tomorrow!";
+  button.style.cursor = "default";
+
+  //add your code here
+button.removeEventListener('click', showChance);
+}
+
+button.addEventListener('click', showChance);
